d07c63d6ea6bd62b4eaccc97ee82f27491ec0dab
Logambigan Thiruganeshapathy <138703319+iamloshan@users.noreply.github.com>
2024-11-24 13:20:22 +0000

    animal- card updated

--- ass4/src/components/AnimalGrid.js
+++ ass4/src/components/AnimalGrid.js
@@ -10,7 +10,18 @@ const AnimalsGrid = ({
     <div className="border-orange">
       <h3>Choose an Animal</h3>
       <div className="animal-grid">
-    
+        {animals.map((animal, index) => (
+          <div
+            key={index}
+            className="animal-card"
+            onClick={() => {
+              setSelectedAnimal(animal.name);
+              checkResult(animal.name);
+            }}
+          >
+
+          </div>
+        ))}
       </div>
     </div>
   );
