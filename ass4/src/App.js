f7598a3dfd30ddac25964bac5cca6764a7e37543
Logambigan Thiruganeshapathy <138703319+iamloshan@users.noreply.github.com>
2024-11-24 12:53:25 +0000

    cinstructing grid

--- ass4/src/App.js
+++ ass4/src/App.js
@@ -1,8 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Animal from "./components/Animal";
 
 function App() {
+  
+    const selectRandomAnimal = () => {
+      const randomIndex = Math.floor(Math.random() * animals.length);
+  
+      return animals[randomIndex].name;
+    };
 
+    function App() {
+      const selectRandomAnimal = () => {
+        const randomIndex = Math.floor(Math.random() * animals.length);
+    
+        return animals[randomIndex].name;
+      };
+
+  return (
+    <div className="container">
+      <header className="border-orange">
+        <h1>Animal Matching Game</h1>
+      </header>
+
+
+      
+    </div>
+  );
 }
 
 export default App;
---
+++ ass4/src/components/AnimalGrid.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AnimalsGrid = ({
+  animals,
+  checkResult,
+  selectedAnimal,
+  setSelectedAnimal,
+}) => {
+  return (
+    <div className="border-orange">
+      <h3>Choose an Animal</h3>
+      <div className="animal-grid">
+    
+      </div>
+    </div>
+  );
+};
+
+export default AnimalsGrid;
