7ba149d2907f46eccd95b5d2002325a105f980dd
Logambigan Thiruganeshapathy <138703319+iamloshan@users.noreply.github.com>
2024-11-24 13:43:01 +0000

    updating app.js file

--- ass4/src/App.js
+++ ass4/src/App.js
@@ -1,21 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Result from "./components/Result";
 import Animal from "./components/Animal";
+import AnimalsGrid from "./components/AnimalsGrid";
+import { animals } from "./data/AnimalsDb";
 
 function App() {
-  
-    const selectRandomAnimal = () => {
-      const randomIndex = Math.floor(Math.random() * animals.length);
-  
-      return animals[randomIndex].name;
-    };
+  const selectRandomAnimal = () => {
+    const randomIndex = Math.floor(Math.random() * animals.length);
 
-    function App() {
-      const selectRandomAnimal = () => {
-        const randomIndex = Math.floor(Math.random() * animals.length);
-    
-        return animals[randomIndex].name;
-      };
+    return animals[randomIndex].name;
+  };
+
+  const [result, setResult] = useState("");
+  const [randomAnimal, setRandomAnimal] = useState(selectRandomAnimal());
+  const [selectedAnimal, setSelectedAnimal] = useState("");
+
+  const checkResult = (selectedAnimal) => {
+    if (selectedAnimal === randomAnimal) {
+      setResult("Win!");
+
+      setTimeout(() => {
+        setResult("");
+        setRandomAnimal(selectRandomAnimal());
+      }, 5000);
+    } else {
+      setResult("Lose!");
+    }
+  };
 
   return (
     <div className="container">
@@ -23,8 +35,22 @@ function App() {
         <h1>Animal Matching Game</h1>
       </header>
 
+      <div className="board">
+        <Result result={result} />
 
-      
+        <Animal
+          animals={animals}
+          randomAnimal={randomAnimal}
+          setRandomAnimal={setRandomAnimal}
+        />
+
+        <AnimalsGrid
+          animals={animals}
+          checkResult={checkResult}
+          selectedAnimal={selectedAnimal}
+          setSelectedAnimal={setSelectedAnimal}
+        />
+      </div>
     </div>
   );
 }
